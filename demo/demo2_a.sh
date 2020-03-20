215b5d444e085ebbc578d1dad626938b0d756c3b
Jessica  Vandebon <jessica.vandebon17@imperial.ac.uk>
2020-03-20 09:44:37 +0000

    adding demo scripts

---
+++ demo/demo2_a.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cd /workspace/apps/adp_test
+python3 /workspace/metaprograms/udt/udt_swi_unroll.py
+
+
+
---
+++ demo/demo2_b.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd apps/adpredictor/swi_project/project
+
+cd device/lib ; make clean ; make   # build HLS kernel
+cd ../..
+source build_kernel.sh              # synthesize hardware
+source program_device.sh            # program FPGA
+make                                # build host
+
+./bin/host 8388608 `pwd`/../../../inputs/adpredictor/   # run program
